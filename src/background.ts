9236cce2634bf1f83c7bc75a638127d99154c013
winebarrel <sugawara@winebarrel.jp>
2024-02-11 16:40:17 +0900

    first commit

---
+++ src/background.ts
@@ -0,0 +1,39 @@
+export async function hideComment() {
+  const approveElem = document.querySelector(
+    "input#pull_request_review\\[event\\]_approve"
+  ) as HTMLInputElement;
+
+  if (!approveElem) {
+    return;
+  }
+
+  const approve = approveElem as HTMLInputElement;
+
+  if (approve.disabled) {
+    return;
+  }
+
+  const commentElem = document.querySelector(
+    "input#pull_request_review\\[event\\]_comment"
+  );
+
+  if (!commentElem) {
+    return;
+  }
+
+  const comment = commentElem as HTMLInputElement;
+  comment.checked = false;
+  comment.parentElement?.remove();
+  approve.checked = true;
+}
+
+const rURL = new RegExp("^https://github.com/[^/]+/[^/]+/pull/[0-9]+/files$");
+
+chrome.tabs.onUpdated.addListener(async (tabId, props, tab) => {
+  if (props.status == "complete" && tab.url?.match(rURL)) {
+    chrome.scripting.executeScript({
+      target: { tabId },
+      func: hideComment,
+    });
+  }
+});
